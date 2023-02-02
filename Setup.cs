eeddba1bc6b20ef3bcf9f4721a0c2119287e62cd
Bruce Devlin <Bruce.Devlin@2itesting.com>
2023-02-02 13:06:50 +0000

    Templated Tool Module

---
+++ Setup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMTASTool
+{
+    /// <summary>
+    /// Called before the user can interact/use this Tool.
+    /// This can be used to setup the environment required for the Tool to run.
+    /// </summary>
+    internal class Setup
+    {
+    }
+}
